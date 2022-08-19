93dfd2351216dfdf2ee71dfb2f74511ec9f870db
Alejandro Vera <aevlanus@gmail.com>
2022-08-19 02:34:55 -0300

    clase 3

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,9 @@
+import "./ItemListContainer.scss"
+
+const ItemListContainer = ( { greetings } ) => {
+  return (
+    <div className="item-list-container">{greetings}</div>
+  )
+}
+
+export default ItemListContainer;
